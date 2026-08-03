26fe8e0847615eaffedec706d10abaf64a5e4868
Chaitanya <chaitanyasonwane2006@gmail.com>
2026-08-04 00:29:18 +0530

    Implement client-side routing with React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import Body from './components/Body.jsx'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./components/Homepage";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
   return (
-      <div>
-            <Body/>
-                </div>
-                  )
-                  }
-                  export default App
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
